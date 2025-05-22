f8c0e44d1e3d5f656f713655774f64572c46dbb5
Leonard Lobel <lenni.lobel@sleektech.com>
2025-05-22 13:52:38 -0500

    VECTORPROPERTY

--- HOL/4. AI Features/04_VectorProperty.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT 
-    Id,
-    VECTORPROPERTY(RawVector, 'Dimension') AS Dimension,
-    VECTORPROPERTY(RawVector, 'Length') AS Length,
-    VECTORPROPERTY(RawVector, 'DataType') AS DataType
-FROM DemoVectors;
